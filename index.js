8ff6af9b72c455c10565c39107a5bdcffa0b93ce
We1kome <40807683+We1kome@users.noreply.github.com>
2026-04-27 13:36:59 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// НАСТРОЙКИ
+const TOKEN = process.env.DISCORD_TOKEN; // ВАЖНО! Токен не пишем тут, берем из Railway
+const TARGET_USER_ID = process.env.TARGET_USER_ID; // ID друга — тоже из Railway
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Бот ${client.user.tag} запущен и следит за другом!`);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.author.id === TARGET_USER_ID) {
+        await message.reply(`⬆️ Пишет дебил. \n> ${message.content}`);
+    }
+});
+
+client.login(TOKEN);
